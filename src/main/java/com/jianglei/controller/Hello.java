8b8a05a01e0928a6b06effcfe9fda4513a80edb4
longyi <longyi@2dfire.com>
2017-03-02 20:58:14 +0800

    框架单元测试通过，但部署运行有问题

--- src/main/java/com/jianglei/controller/Hello.java
+++ src/main/java/com/jianglei/controller/Hello.java
@@ -1,9 +1,13 @@
 package com.jianglei.controller;
 
+import com.jianglei.entity.User;
+import com.jianglei.mapper.UserMapper;
+import com.jianglei.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,14 +18,19 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/hello")
 public class Hello {
+//    @Resource
+//    UserService userService;
+    @Resource
+    UserMapper userMapper;
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public void index(HttpServletRequest request, HttpServletResponse response){
 
-    //    User user = userService.getUserByName("jianglei");
+        //User user = userService.getUserByName("jianglei");
+        User user = userMapper.getUser("jianglei");
 
         try {
             //response.getWriter().write(user.getName()+"  "+user.getAge());
-            response.getWriter().write("hello world");
+            response.getWriter().write(user.getName()+" "+user.getAge());
         } catch (IOException e) {
             e.printStackTrace();
         }
